9a206605a7bdb63cf7a63eae753c86d3648dd14b
abhishek-hyangol <abhishekhyangol123@gmail.com>
2025-06-26 12:29:09 +0545

    Small Updates

---
+++ src/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde repudiandae qui officiis ut possimus, expedita voluptatum veritatis nesciunt alias ab.</p>
+        </div>
+    )
+}
+
+export default Hero
